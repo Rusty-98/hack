80ad2c110884922c5f5a494269cbf81ab4bc062d
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-05-31 23:14:50 +0530

    done some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,19 +63,19 @@ const Navbar = () => {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul className="flex text-lg flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
+                            <Link to={"/"} className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
                         </li>
                         <li>
-                            <Link href="/project" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Projects</Link>
+                            <Link to={"/project"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Projects</Link>
                         </li>
                         <li>
-                            <Link href="/team" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Team</Link>
+                            <Link to={"/team"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Team</Link>
                         </li>
                         <li>
-                            <Link href="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</Link>
+                            <Link to={"/about"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</Link>
                         </li>
                         <li>
-                            <Link href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact Us</Link>
+                            <Link to={"/contact"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact Us</Link>
                         </li>
                     </ul>
                 </div>
@@ -83,19 +83,19 @@ const Navbar = () => {
                     <div className={`${show ? "show" : "hide"} h-screen w-full overflow-hidden bg-gray-900 flex flex-col items-center`}>
                         <ul className="flex text-4xl flex-col p-4 justify-evenly h-[60%]">
                             <li>
-                                <Link href="/" className="text-white">Home</Link>
+                                <Link to={"/"} className="text-white">Home</Link>
                             </li>
                             <li>
-                                <Link href="/project" className="text-white">Projects</Link>
+                                <Link to={"/project"} className="text-white">Projects</Link>
                             </li>
                             <li>
-                                <Link href="/team" className="text-white">Team</Link>
+                                <Link to={"/team"} className="text-white">Team</Link>
                             </li>
                             <li>
-                                <Link href="/about" className="text-white">About Us</Link>
+                                <Link to={"/about"} className="text-white">About Us</Link>
                             </li>
                             <li>
-                                <Link href="/contact" className="text-white">Contact Us</Link>
+                                <Link to={"/contact"} className="text-white">Contact Us</Link>
                             </li>
                         </ul>
                     </div>
