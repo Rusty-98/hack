d2e914231759edaeb7b402d5e3425fd23bde9dc4
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-06-01 20:14:33 +0530

    fixed register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -10,7 +10,6 @@ const Register = () => {
         name: '',
         email: '',
         password: '',
-        userType: 'Prop' // Default value is 'Prop'
     });
 
     const notify = () => toast.success('✅️ Registered!', {
@@ -77,13 +76,6 @@ const Register = () => {
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" value={formData.password} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                                </div>
-                                <div>
-                                    <label htmlFor="userType" className="block mb-2 text-sm font-medium text-white">User Type</label>
-                                    <select name="userType" id="userType" value={formData.userType} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                                        <option value="Prop">Prop</option>
-                                        <option value="Stud">Stud</option>
-                                    </select>
                                 </div>
                                 <button type="submit" className="w-full text-white bg-blue-700 border border-white hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register</button>
                                 <p className="text-sm text-gray-300">
